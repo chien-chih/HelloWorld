3f4e5aa3b82c2a74b0c3c5d6b4141aa2cf2847e7
chien-chih <791088@gmail.com>
2018-01-24 16:10:54 -0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "HelloWorld"
+
+version := "0.1"
+
+scalaVersion := "2.11.12"
+
+libraryDependencies += "com.typesafe.akka" %  "akka-http_2.11" % "10.0.5"
---
+++ src/main/scala/com/strategycompanion/HelloHttpServer.scala
@@ -0,0 +1,27 @@
+package com.strategycompanion
+
+import akka.http.scaladsl.server.HttpApp
+
+object HelloHttpServer extends HttpApp{
+
+  def route =
+    pathPrefix("v1") {
+      path("id" / Segment) { id =>
+        get {
+          println("server get " + id)
+          complete(s"Hello World from akka")
+        } ~
+          post {
+            entity(as[String]) { entity =>
+              println("server get " + entity)
+              complete(s"got post request")
+            }
+          }
+      }
+    }
+
+}
+
+object HelloHttpServerApplication extends App {
+  HelloHttpServer.startServer("0.0.0.0", 8099)
+}
